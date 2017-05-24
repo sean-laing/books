67e94ef7651e52fa82613b7c430cdd2f658dde8a
Sean Laing <seanlain@seanlain-macbook.local>
2017-05-24 09:44:02 -0700

    added more books

--- rename.bash
+++ rename.bash
@@ -1,1 +1,13 @@
-rename.bash
+#!/bin/bash
+
+echo $@
+booktypes=()
+rm intermediate_data/raw_word_counts.tsv
+
+for var in "$@"
+do
+	echo $var
+	base=`dirname $var`
+	bookname=`head -n 1 $var | tr ' ' '_' |  tr -d '\n'` #|  sed 's/The Project Gutenberg EBook of //g' 
+	mv $var base/$bookname
+done
