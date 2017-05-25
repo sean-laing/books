788a4731c91ab9a2ab22d37679704774cda3503e
Sean Laing <seanlain@seanlain-macbook.local>
2017-05-25 10:50:33 -0700

    added html generation autmation to process_books.bash

--- process_books.bash
+++ process_books.bash
@@ -17,4 +17,5 @@ done
 unique_booktype=`echo ${booktypes[@]} | tr ' ' '\n' | sort -u | tr '\n' ' '`
 echo $unique_booktype
 cat intermediate_data/raw_word_counts.tsv | node get_dense_columns.js  > intermediate_data/features.tsv
-cat intermediate_data/raw_word_counts.tsv | node get_dense_columns.js | ./rf_train_validate.R $unique_booktype > prediction/all_prediction.tsv 2> prediction/accuracy.tsv
+cat intermediate_data/features.tsv | ./rf_train_validate.R $unique_booktype > prediction/all_prediction.tsv 2> prediction/accuracy.tsv
+cat prediction/all_prediction.tsv | node create_html.js --booktypes romance war horror fantasy > output.html && open output.html
